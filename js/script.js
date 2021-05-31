dda8cd59ccdae46d7857a76e591a976f48fd29d2
vvalcarcell <valcarcellvalentina@gmail.com>
2021-05-31 16:30:59 +0200

    winner

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ var userEmail = prompt('Inserisci la tua email');
 
 var check = false;
 
+// var userResult = Math.floor(Math.random() * 10);
+// var computerResult = Math.floor(Math.random() * 10);
+
 for (var i = 0; i < eMail.length; i++) {
     if (userEmail === eMail[i]) {
         check = true;
@@ -20,4 +23,11 @@ function alea() {
     document.getElementById('start').innerHTML = 'Alea Iacta Est!'
     document.getElementById('user-result').innerHTML = Math.floor(Math.random() * 10);
     document.getElementById('computer-result').innerHTML = Math.floor(Math.random() * 10);
+}
+
+if (parseInt(user - result) > parseInt(computer - result)) {
+    document.getElementById('winner').innerHTML = 'Complimenti! Hai vinto!';
+} else {
+    document.getElementById('winner').innerHTML = 'Oh no! Purtroppo hai perso!';
+
 }
